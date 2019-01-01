637d4b85b9cfbbdb0cf5468ac5b670c3bb76730c
Francesco Abbate <francesco.bbt@gmail.com>
2019-01-01 23:06:53 +0100

    Implement a mathematical function to plot

--- PrimitivesExample.cpp
+++ PrimitivesExample.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <Magnum/GL/Buffer.h>
 #include <Magnum/GL/DefaultFramebuffer.h>
 #include <Magnum/GL/Mesh.h>
@@ -11,6 +12,43 @@
 
 using namespace Magnum;
 using namespace Magnum::Math::Literals;
+
+template <typename F, typename dF>
+Trade::MeshData3D mathFunctionMeshData(float x_min, float x_max, float y_min, float y_max, F evalF, dF evaldF) {
+    const int i_min = -10, i_max = 10;
+    const int j_min = -10, j_max = 10;
+    std::vector<Vector3> positions{};
+    std::vector<Vector3> normals{};
+    for (int j = j_min; j <= j_max; j++) {
+        for (int i = i_min; i <= i_max; i++) {
+            const float x = x_min + (i - i_min) * (x_max - x_min) / (i_max - i_min);
+            const float y = y_min + (j - j_min) * (y_max - y_min) / (j_max - j_min);
+            const float z = evalF(x, y);
+            const Vector2 df = evaldF(x, y);
+            const float nf = std::sqrt(df.x() * df.x() + df.y() * df.y() + 1.0f);
+            positions.push_back(Vector3{x, y, z});
+            normals.push_back(Vector3{-df.x() / nf, -df.y() / nf, 1 / nf});
+        }
+    }
+
+    auto index = [&](int i, int j) { return (j - j_min) * (i_max - i_min + 1) + (i - i_min); };
+
+    std::vector<UnsignedInt> indices{};
+    for (int j = j_min; j <= j_max - 1; j++) {
+        for (int i = i_min + 1; i <= i_max; i++) {
+            indices.push_back(index(i - 1, j));
+            indices.push_back(index(i - 1, j + 1));
+            indices.push_back(index(i, j));
+
+            indices.push_back(index(i - 1, j + 1));
+            indices.push_back(index(i, j + 1));
+            indices.push_back(index(i, j));
+        }
+    }
+
+    return Trade::MeshData3D{MeshPrimitive::Triangles,
+        indices, {positions}, {normals}, {}, {}, nullptr};
+}
 
 class MyApp: public Platform::Application {
     public:
@@ -32,12 +70,21 @@ class MyApp: public Platform::Application {
 };
 
 MyApp::MyApp(const Arguments& arguments):
-    Platform::Application{arguments, Configuration{}.setTitle("Magnum Primitives Example")}
+    Platform::Application{arguments, Configuration{}.setTitle("My Magnum Testing App")}
 {
     GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
-    GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
+    // GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
 
-    const Trade::MeshData3D cube = Primitives::cubeSolid();
+    // const Trade::MeshData3D cube = Primitives::cubeSolid();
+    const float gauss_c = 5.0f;
+    auto f = [&](float x, float y) {
+        return std::exp(-gauss_c * (x*x + y*y));
+    };
+    auto df = [=](float x, float y) {
+        const float e = std::exp(-gauss_c * (x*x + y*y));
+        return Vector2{-2*gauss_c*x*e, -2*gauss_c*y*e};
+    };
+    const Trade::MeshData3D cube = mathFunctionMeshData(-1.0, 1.0, -1.0, 1.0, f, df);
 
     _vertexBuffer.setData(MeshTools::interleave(cube.positions(0), cube.normals(0)));
 
